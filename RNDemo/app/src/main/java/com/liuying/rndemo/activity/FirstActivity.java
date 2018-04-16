77d76ff1c199f58e4244f3adc68bf964f477226c
liuying <ly@btjf.com>
2018-04-16 16:58:30 +0800

    update .gitignore

---
+++ RNDemo/app/src/main/java/com/liuying/rndemo/activity/FirstActivity.java
@@ -0,0 +1,15 @@
+package com.liuying.rndemo.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liuying on 2018/3/30 15:10.
+ * Email: ly1203575492@163.com
+ */
+public class FirstActivity extends AppCompatActivity {
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
